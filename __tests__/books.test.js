f2dc7829403fdc13304795a1d66c0569c5b162c1
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-16 11:57:42 -0700

    getById passes test

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,6 +1,6 @@
 const setup = require('../data/setup');
 const app = require('../lib/app');
-const { request } = require('../lib/app');
+const request = require('supertest');
 const pool = require('../lib/utils/pool');
 
 describe('books routes testing', () => {
@@ -11,6 +11,12 @@ describe('books routes testing', () => {
   it('should return a list of books', async () => {
     const results = await request(app).get('/books');
     expect(results.status).toEqual(200);
+    expect(results.body.length).toEqual(4);
+  });
+
+  it('should return a single book based on the id params', async () => {
+    const results = await request(app).get('/books/1');
+    expect(results.body.title).toEqual('The Monster at the end of this book');
   });
 
   afterAll(() => {
