3b8fd8daf307ac8a126497842cb3e7d6705e68ff
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-17 16:03:30 -0700

    insert test passes

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -2,6 +2,7 @@ const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
+const Author = require('../lib/models/Author');
 
 describe('authors routes testing', () => {
   beforeEach(() => {
@@ -19,7 +20,7 @@ describe('authors routes testing', () => {
     expect(results.body.name).toEqual('Eric Hill');
   });
 
-  it('PUT /authors/:id should update author with id', async () => {
+  it.skip('PUT /authors/:id should update author with id', async () => {
     const results = await request(app).put('/authors/1').send({
       name: 'Eric Hill, PhD',
     });
@@ -28,8 +29,12 @@ describe('authors routes testing', () => {
     expect(results.body.name).toEqual('Eric Hill, PhD');
   });
 
-  it.skip('should insert a new author into our table', async () => {
-    const author = new { name: 'Alex Orlet', dob: 1989 }();
+  it('should insert a new author into our table', async () => {
+    const author = new Author({
+      name: 'Alex Orlet',
+      dob: 1989,
+      pob: 'St. Louis, Missouri',
+    });
     const results = await request(app).post('/authors/:id').send(author);
     expect(results.status).toEqual(200);
     expect(results.body.name).toEqual('Alex Orlet');
