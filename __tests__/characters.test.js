986a2f42bc248cc576b8c140f3289744f74368c7
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-17 16:59:10 -0700

    created get tests for characters

---
+++ __tests__/characters.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+const setup = require('../data/setup');
+
+describe('characters routes testing', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it.skip('should return a list of characters', async () => {
+    const results = await request(app).get('/characters');
+    expect(results.status).toEqual(200);
+    expect(results.body.length).toEqual(4);
+  });
+
+  it.skip('should return a single character based on the id params', async () => {
+    const results = await request(app).get('/characters/1');
+    expect(results.body.first_name).toEqual('Moira');
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
