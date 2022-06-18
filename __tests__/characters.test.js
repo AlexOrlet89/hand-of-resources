e16b88eb7259430a045279b2bfba1d2048b4ed8b
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-17 17:20:14 -0700

    Route for insert, method for insert, insert test passes

--- __tests__/characters.test.js
+++ __tests__/characters.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const setup = require('../data/setup');
+const Character = require('../lib/models/Character');
 
 describe('characters routes testing', () => {
   beforeEach(() => {
@@ -19,13 +20,23 @@ describe('characters routes testing', () => {
     expect(results.body.first_name).toEqual('Moira');
   });
 
-  it('PUT /characters/:id should update character with id', async () => {
+  it.skip('PUT /characters/:id should update character with id', async () => {
     const results = await request(app).put('/characters/1').send({
       first_name: 'Moira',
     });
     console.log(results.body);
     expect(results.status).toEqual(200);
     expect(results.body.first_name).toEqual('Moira');
+  });
+
+  it('should insert a new character into our table', async () => {
+    const character = new Character({
+      first_name: 'Alex Orlet',
+      last_name: 'Orlet Orlet',
+    });
+    const results = await request(app).post('/characters/:id').send(character);
+    expect(results.status).toEqual(200);
+    expect(results.body.first_name).toEqual('Alex Orlet');
   });
 
   afterAll(() => {
